ab4599cbb65225cd5515eaf124c238b270cd0ade
Ernest Cheung <ernestcheung02@gmail.com>
2021-10-31 21:59:30 -0700

    Fixed location error

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { HashRouter as Router, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import SlideRoutes from 'react-slide-routes';
 // Components
@@ -9,7 +9,6 @@ import Hero from './HeroMain/Hero';
 import Main from './Main/Main';
 
 function App() {
-	const location = useLocation();
 	return (
 		<div className="App">
 			{/* <Route
